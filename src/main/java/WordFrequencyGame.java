695c4a8efd967cd98fb224808b360dfdb617bc3c
YANGKI4 <dunka97@163.com>
2020-07-27 15:44:30 +0800

    [KikiYang]refactor : using const replace the four magic number,like'\\s+''\n''Calculate Error'' '

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,17 +5,24 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class WordFrequencyGame {
+
+    public static final String SPACE_PATTERN = "\\s+";
+    public static final String LINE_BREAKS = "\n";
+    public static final String CALCULATE_ERROR = "Calculate Error";
+    public static final String SPACE_STRING = " ";
+
     public String getResult(String inputStr) {
 
 
-        if (inputStr.split("\\s+").length==1) {
+
+        if (inputStr.split(SPACE_PATTERN).length==1) {
             return inputStr + " 1";
         } else {
 
             try {
 
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = inputStr.split(SPACE_PATTERN);
 
                 List<WordInfo> wordInfoList = new ArrayList<>();
                 for (String s : arr) {
@@ -35,14 +42,14 @@ public class WordFrequencyGame {
 
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                StringJoiner joiner = new StringJoiner("\n");
+                StringJoiner joiner = new StringJoiner(LINE_BREAKS);
                 for (WordInfo w : wordInfoList) {
-                    String s = w.getValue() + " " +w.getWordCount();
+                    String s = w.getValue() + SPACE_STRING +w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
             } catch (Exception e) {
-                return "Calculate Error";
+                return CALCULATE_ERROR;
             }
         }
     }
